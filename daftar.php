fd9b2d8f342b50a931df654cbfb6a1bd9f98c956
unknown <hanifah.les@gmail.com>
2019-12-04 14:23:27 +0700

    Test

---
+++ daftar.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Vacation - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">TODAY<span>TOUR GUIDE FOR HOLIDAY</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+					<li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+					<li class="nav-item"><a href="destination.php" class="nav-link">Destination</a></li>
+					<li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
+					<li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+					<li class="nav-item cta"><a href="login.php" class="nav-link">Login</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="hero-wrap js-fullheight" style="background-image: url('images/header.jpg');"
+        data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center"
+                data-scrollax-parent="true">
+
+                <section class="ftco-section contact-section">
+                    <br><br>
+                    <div class="container">
+                        <div class="row block-9">
+                            <div class="col-md-6 order-md-last d-flex">
+                                <form action="#" class="bg-light p-5 contact-form">
+                                    <div class="form-group">
+                                    <label for="inputnama">Name</label>
+                                        <input type="text" class="form-control" placeholder="Your Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <!-- <div class="form-group">
+                                        <label for="inputalamat">Address</label>
+                                            <input type="text" class="form-control" placeholder="Address">
+                                        </div> -->
+                                        <label for="inputemail">Email</label>
+                                        <input type="text" class="form-control" placeholder="Your Email">
+                                    </div>
+                                    <div class="form-group">
+                                    <label for="inputpass">Password</label>
+                                        <input type="int" class="form-control" placeholder="password">
+                                    </div>
+                                    <div class="form-group">
+                                    <label>As be</label>
+                                        <select id="inputpilihan" class="form-control" >
+                                       
+                                            <option>Tour Guide</option>
+                                            <option>Traveller</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <center><input type="submit" value="Sign Up" class="btn btn-primary py-3 px-5">
+                                        </center>
+                                    </div>
+
+                                </form>
+
+                            </div>
+
+                            <div class="col-md-3 d-flex"></div>
+                        </div>
+                    </div>
+                </section>
+
+
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/scrollax.min.js"></script>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
